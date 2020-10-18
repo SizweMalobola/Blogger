6ae334d4d6fc009c67a33d7cb09c3edbb7613bff
SizweMalobola <25reyals@gmail.com>
2020-10-18 21:32:22 +0200

    tagging feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ApplicationRecord
-    has_many :comments
+    has_many :taggings
+    has_many :tags, through: :taggings, dependent: :destroy
+    has_many :comments, dependent: :destroy
+
+    def tag_list=(names)
+        self.tags = names.downcase.split(',').uniq.map do |name|
+            Tag.where(name: name).first_or_create!
+        end
+    end
+
+    def tag_list
+        tags.map(&:name).join(', ')
+    end
 end
